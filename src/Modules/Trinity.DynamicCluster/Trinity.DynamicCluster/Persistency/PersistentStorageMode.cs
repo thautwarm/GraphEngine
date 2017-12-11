8387331a91bb2d9ae6fbed418453b84da8e1fc16
Yatao Li <yatli@microsoft.com>
2017-12-11 17:33:50 +0800

    Update PersistentStorageMode.cs
    Make the enum composable, e.g. `Local | VolatileMemory | AddressSpace` or `Replication | MechanicalDrive | FileSystem`

--- src/Modules/Trinity.DynamicCluster/Trinity.DynamicCluster/Persistency/PersistentStorageMode.cs
+++ src/Modules/Trinity.DynamicCluster/Trinity.DynamicCluster/Persistency/PersistentStorageMode.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace Trinity.DynamicCluster.Persistency
 {
-    public enum PersistentStorageMode
+    [Flags]
+    public enum PersistentStorageMode : int
     {
+        /* Locality */
+        Local,
+        Replication,
+        External,
+        /* Medium */
         // TODO: Review this collection and complete the list.    
-        BatteriedMemory,
-        Memory,
-        SolidStateDisk,
-        HardDisk
+        VolatileMemory,
+        NonVolatileMemory,
+        MechanicalDrive,
+        SolidStateDrive,
+        RowStore,
+        /* Accessibility */
+        FileSystem, // Azure Blob Storage, HDFS, etc.
+        RowStore,
+        AddressSpace, // Memory, buffer, etc.
     }
 }
